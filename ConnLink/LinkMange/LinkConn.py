19d93a5d9e2ee5068c873bb271d60377707dda36
pavan-0328 <pavansaikumarreddy354@gmail.com>
2024-08-31 16:31:42 +0530

    Tested multy vehicle control

--- ConnLink/LinkMange/LinkConn.py
+++ ConnLink/LinkMange/LinkConn.py
@@ -55,7 +55,7 @@ def CheckAvailablePorts():
     #returns all drones online
     available = []
     available = CheckAvailableSerial()
-    
+    online = []
     for port in available:
         print(port.device)
         try:
@@ -64,10 +64,12 @@ def CheckAvailablePorts():
             if latency != -1.0:
                 print(f"Drone is connected to port: {port.device}")
                 print(f"Connection latency: {latency * 1000.0}ms")
+                online.append(port.device)
             else:
                 print(f"Connection latency: {latency}")
         except Exception as e:
             print(f"No heartbeat received or connection error: {e}")
+    return online
        
 def send_heartbeat(mav):
     mav.mav.heartbeat_send(
@@ -93,7 +95,7 @@ def armDrone(port):
 
 def CheckConnHealth(port):
     # Send heartbeat and measure time until response is received
-    armDrone(port)
+   
     mav = mavutil.mavlink_connection(port.device, baud=57600, timeout=4)
     send_heartbeat(mav)
     
@@ -110,8 +112,38 @@ def CheckConnHealth(port):
             return -1
 
 def EstablishConn():
-    pass
+    online = CheckAvailablePorts()
+    print(f"DRONE online {len(online)}")
+    droneDict = {}
+    cnt = 1
+    for port in online:
+        temp_conn = mavutil.mavlink_connection(port, baud=57600, timeout=4)
+        droneDict[cnt] = temp_conn
+        cnt+=1
+    cnt = 1
+    for drone in droneDict.values():
+        print(cnt)
+        mode = drone.mode_mapping()['STABILIZE']
+        drone.mav.set_mode_send(
+            drone.target_system,
+            mavutil.mavlink.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED,
+            mode)
+        time.sleep(2)
+        #heartbeat_msg = drone.recv_match(type='HEARTBEAT', blocking=True)
+        #print(heartbeat_msg.custom_mode)
+        drone.mav.command_long_send(
+            drone.target_system,
+            drone.target_component,
+            mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
+            0, #configuration
+            1, #Arm, disarm
+            0,0,0,0,0,0 #parameters
+        )
+        time.sleep(5)
+        cnt+=1
+    print("Done")
+        
 
-CheckAvailablePorts()
+EstablishConn()
 
 
